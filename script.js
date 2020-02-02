dc5d0da01583313349fdb4d48de089ba480b2f77
JosephAlba19 <jalba19@gmail.com>
2020-02-01 22:50:28 -0500

    Completed the email and username validation using vanilla js

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+// Check email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // Even listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -26,5 +32,26 @@ form.addEventListener('submit', function(e) {
         showError(username, 'Username is required');
     } else {
         showSuccess(username);
+    }
+
+    if(email.value === '') {
+        showError(email, 'Email is required');
+    } else if(!isValidEmail(email.value)) {
+        showError(email,'Email is not valid')
+    } 
+    else {
+        showSuccess(email);
+    }
+
+    if(password.value === '') {
+        showError(password, 'Username is required');
+    } else {
+        showSuccess(password);
+    }
+
+    if(password2.value === '') {
+        showError(password2, 'Username is required');
+    } else {
+        showSuccess(password2);
     }
 });
